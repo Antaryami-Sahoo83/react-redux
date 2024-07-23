49b49d7eb95c114581c2f59615786465e596cf96
Antaryami Sahoo <mail2sahooantaryami@gmail.com>
2024-07-23 20:31:12 +0530

    product display using redux

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,33 @@
+import { ActionTypes } from "../constants/action-types";
+
+const initiaState = {
+      products:[],
+}
+
+
+export const productReducer = (state = initiaState , {type, payload}) => {
+      switch(type){
+            case ActionTypes.SET_PRODUCTS:
+                  return {...state, products:payload};
+            default:
+                  return state;
+      }
+}
+
+export const selectedProductReducer = (state={}, {type, payload}) => {
+      switch(type){
+            case ActionTypes.SELECTED_PRODUCT:
+                  return {...state, ...payload};
+            default:
+                  return state;
+      }
+}
+
+export const removeselectedProductReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.REMOVE_SELECTED_PRODUCT:
+      return {};
+    default:
+      return state;
+  }
+};
